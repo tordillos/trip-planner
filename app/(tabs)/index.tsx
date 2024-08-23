612140e2449d23c650e9119fd697e5db21d69ed3
Pedro Blázquez <pedro.blazquez@wembleystudios.com>
2024-08-23 18:17:29 +0200

    feat: add text component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,21 @@
-import { Image, StyleSheet } from 'react-native';
+import { Image, StyleSheet } from "react-native";
 
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { Sun } from '@/lib/icons/Sun';
-import { Text } from 'react-native';
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { Sun } from "@/lib/icons/Sun";
+import { Text } from "@/components/ui/text";
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
-      }>
-      <Text className='text-white text-2xl'>Welcome to Expo!</Text>
+      }
+    >
+      <Text className="text-2xl">Welcome to Expo!</Text>
       <Sun />
     </ParallaxScrollView>
   );
@@ -22,8 +23,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -35,6 +36,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
